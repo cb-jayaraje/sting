5ea9fa61b50ec0f3801e5955cac119986e79797e
cb-deveshpawar <devesh.pawar@chargebee.com>
2022-08-01 17:08:26 +0530

    added checkbox, tabs, toggles components

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -3,6 +3,9 @@ import { concat, from, map } from 'rxjs';
 import { inputGroupsService } from 'src/app/services/input-groups.service';
 import { SelectMenuServices } from 'src/app/services/select-menu.service';
 import { SignInFormsService } from 'src/app/services/sign-in-forms.service';
+import { CheckboxService } from 'src/app/services/checkbox.service';
+import { RadioGroupService } from 'src/app/services/radio-groups.service';
+import { ToggleService } from 'src/app/services/toggle.service';
 import { getNum } from 'src/app/util';
 import * as formsData from './../../data-page.json';
 
@@ -13,10 +16,10 @@ import * as formsData from './../../data-page.json';
 })
 export class FormsComponent implements OnInit {
 
- 
-  formComponents:any = [];
+
+  formComponents: any = [];
   formData: any = formsData;
-  imageUrl= "./assets/images/";
+  imageUrl = "./assets/images/";
   path = '/components/page/'
 
   testdata: any = formsData;
@@ -39,7 +42,7 @@ export class FormsComponent implements OnInit {
     //   //   console.log(`${key}: ${value}`);
     //   // }
     //  this.te += tr
-      
+
     // });
 
 
@@ -54,25 +57,28 @@ export class FormsComponent implements OnInit {
     //   // newArr.forEach((element: any) => {
     //   //   element.forEach( y => console.log(y))
     //   // });
-      
+
 
     //   return newArr;
-  
-      
+
+
     // }
     // )).subscribe(data=> console.log(data))
 
-   
+
 
 
     // this.formData = formsData
-    
+
 
     // this.formComponents = this.formData.forms;
-      this.formComponents = [
-        {"title": "Input Groups", "noOfComponents": getNum(inputGroupsService), "image":  "input-groups.png", "link": "input-groups"},
-        {"title": "Select Menus", "noOfComponents": getNum(SelectMenuServices), "image": "select-menus.png", "link": "select-menus"},
-        {"title": "Sign-in and Registration", "noOfComponents": getNum(SignInFormsService), "image": "sign-in-forms.png", "link": "sign-in-forms"}
+    this.formComponents = [
+      { "title": "Input Groups", "noOfComponents": getNum(inputGroupsService), "image": "input-groups.png", "link": "input-groups" },
+      { "title": "Select Menus", "noOfComponents": getNum(SelectMenuServices), "image": "select-menus.png", "link": "select-menus" },
+      { "title": "Sign-in and Registration", "noOfComponents": getNum(SignInFormsService), "image": "sign-in-forms.png", "link": "sign-in-forms" },
+      { "title": "Checkboxes", "noOfComponents": getNum(CheckboxService), "image": "checkboxes.png", "link": "checkbox" },
+      { "title": "Radio Buttons", "noOfComponents": getNum(RadioGroupService), "image": "radio-groups.png", "link": "radio-groups" },
+      { "title": "Toggles", "noOfComponents": getNum(ToggleService), "image": "toggles.png", "link": "toggles" },
     ];
 
     // console.log(this.formData.forms)
---
+++ sting-components/tabs.js
@@ -0,0 +1,22 @@
+module.exports = Tabs = (styles) => ({
+
+    '.tab-link': {
+        '@apply border-transparent whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm': {}
+    },
+    '.tab-link_active': {
+        '@apply border-indigo-500 text-indigo-600 border-solid': {}
+    },
+    '.tab-link_inactive': {
+        '@apply text-gray-500 hover:text-gray-700 hover:border-gray-300': {}
+    },
+    '.tab-pill': {
+        '@apply px-3 py-2 font-medium text-sm rounded-md': {}
+    },
+    '.tab-pill_active': {
+        '@apply bg-gray-100 text-gray-700': {}
+    },
+    '.tab-link_badges': {
+        '@apply hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block': {}
+    },
+
+});

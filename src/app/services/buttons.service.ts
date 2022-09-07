0425a220e3b6fc439a3670f75423f65df01fad57
cb-deveshpawar <devesh.pawar@chargebee.com>
2022-09-07 18:30:13 +0530

    component updates

--- src/app/services/buttons.service.ts
+++ src/app/services/buttons.service.ts
@@ -123,7 +123,7 @@ export class ButtonService {
 
             <!-- Link Button starts -->
             <a href="#link" class="cb-text-copy-regular btn-link flex">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1 -ml-0.5" viewBox="0 0 20 20"
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-1 -ml-0.5" viewBox="0 0 20 20"
                 fill="currentColor">
                 <path
                   d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
@@ -135,7 +135,7 @@ export class ButtonService {
             <!-- Link Button starts -->
             <a href="#link" class="cb-text-copy-regular btn-link flex">
               <span>Link</span>
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1 -mr-0.5" viewBox="0 0 20 20"
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1 -mr-0.5" viewBox="0 0 20 20"
                 fill="currentColor">
                 <path
                   d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
@@ -153,7 +153,7 @@ export class ButtonService {
 
             <!-- Link Button starts -->
             <a href="#link" class="cb-text-copy-small btn-link flex">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1 -ml-0.5" viewBox="0 0 20 20"
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5 mr-1 -ml-0.5" viewBox="0 0 20 20"
                 fill="currentColor">
                 <path
                   d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
@@ -165,7 +165,7 @@ export class ButtonService {
             <!-- Link Button starts -->
             <a href="#link" class="cb-text-copy-small btn-link flex">
               <span>Link</span>
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1 -mr-0.5" viewBox="0 0 20 20"
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5 ml-1 -mr-0.5" viewBox="0 0 20 20"
                 fill="currentColor">
                 <path
                   d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />

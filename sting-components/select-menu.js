71b86d80491d020a0e6b43463a916ff6c5779b70
Gianni James Favaretto <giannijfavaretto@gmail.com>
2022-10-29 09:51:32 +0530

    select menu update

--- sting-components/select-menu.js
+++ sting-components/select-menu.js
@@ -1,43 +1,40 @@
 module.exports = SelectMenus = (styles) => ({ 
-    '.form-control .select': {
-        "@apply block w-full pl-3 pr-10 py-2 text-copy-regular text-neutral-700 border-neutral-400 focus:outline-none focus:!ring-primary-500 focus:!border-primary-500 sm:text-sm rounded shadow-sm": {},
-         },
-         '.form-control .select-button': {
-        "@apply bg-white  relative w-full border text-copy-regular text-neutral-700 border-neutral-400  rounded shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:!ring-primary-500 focus:!border-primary-500 sm:text-sm": {},
-         },
-         '.form-control .select-button-l-curve': {
-          "@apply relative inline-flex items-center bg-primary-500 p-2 rounded-l-none rounded-r-md text-sm font-medium text-white hover:bg-primary-500 focus:outline-none focus:z-10 focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-primary-500": {},
-           },
-         '.theme-sting .form-control .select-button': { 
-          "@apply block w-full pl-3 pr-10 py-2 text-copy-regular text-neutral-700 border-neutral-400 focus:outline-none focus:!ring-primary-500 focus:!border-primary-500 sm:text-sm rounded shadow-sm": {},
-
-         },
-         '.form-control .select-button-icon-container': {
+     '.form-control .select': {
+          "@apply block w-full pl-3 pr-10 py-2 cb-text-copy-regular border border-neutral-400 focus:outline-none focus:!ring-primary-500 focus:!border-primary-500 sm:text-sm rounded shadow-sm": {},
+     },
+     '.form-control .select-button': {
+          "@apply bg-white relative w-full cb-text-copy-regular border border-neutral-400 rounded shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:!ring-primary-500 focus:!border-primary-500 sm:text-sm": {},
+     },
+     '.form-control .select-button span:first-class': {
+          "@apply block truncate": {},
+     },
+     '.form-control .select-button span:last-of-type': {
           "@apply absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none": {},
-         },
-         '.form-control .select-ul': {
-        "@apply absolute z-10 w-full bg-white shadow-lg max-h-60 rounded  text-copy-regular ring-1 ring-primary-500 ring-opacity-5 overflow-auto focus:outline-none sm:text-sm": {},
-             },
-        '.form-control .select-li': {
-        "@apply text-neutral-700 cursor-default select-none relative py-2 pl-3 pr-9 hover:bg-neutral-100": {},
-             },
-
-             '.theme-sting .form-control .select-li': {
-               "@apply text-neutral-700 hover:text-neutral-800 cursor-default select-none relative py-2 pl-3 pr-9 hover:bg-neutral-100": {},
-                    },
-                    '.form-control .select-li-selected': {
-                         "@apply text-info-800 bg-info-50 cursor-default select-none relative py-2 pl-3 pr-9 hover:bg-info-50": {},
-                              },
-
-        '.form-control .select-li-icon': {
-        "@apply !text-info-800 absolute inset-y-0 flex items-center right-0 pr-4": {},
-             }, 
-
-            
-
-        '.form-control .select-secondary-text': {
-        "@apply truncate text-gray-700": {},
-             }, 
+     },
+     '.form-control .select-button svg': {
+          "@apply w-5 h-5": {},
+     },
+     '.form-control .select-ul': {
+          "@apply absolute z-10 w-full bg-white mt-1 shadow-lg max-h-60 rounded ring-1 ring-primary-500 ring-opacity-5 overflow-auto focus:outline-none": {},
+     },
+     '.form-control .select-ul li': {
+          "@apply cb-text-copy-regular cursor-default select-none relative py-2 pl-3 pr-9 hover:bg-neutral-100": {},
+     },
+     '.form-control .select-ul li span:first-class': {
+          "@apply block truncate": {},
+     },
+     '.form-control .select-ul li span:nth-child(2)': {
+          "@apply absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none": {},
+     },
+     '.form-control .select-ul li span:nth-child(2) svg': {
+          "@apply w-5 h-5": {},
+     },
+     '.form-control .select-li-selected': {
+          "@apply cb-text-copy-regular text-info-800 bg-info-50 cursor-default select-none relative py-2 pl-3 pr-9 hover:bg-info-50": {},
+     },
+     '.form-control .select-ul li.select-li-selected span:nth-child(2) svg': {
+          "@apply !text-info-800": {},
+     },
        
         
 })

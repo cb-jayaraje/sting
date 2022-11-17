896ad275b00b1c5acbeea9a19dc68af2119c4946
Gianni James Favaretto <giannijfavaretto@gmail.com>
2022-11-17 09:27:36 +0000

    small updates

--- src/app/services/toggle.service.ts
+++ src/app/services/toggle.service.ts
@@ -36,7 +36,7 @@ export class ToggleService {
       
         {
             id: "toggle_2",
-            title: "With left label and description",
+            title: "Default",
             code: 
                 `
                 <div class="form-control">
@@ -77,12 +77,12 @@ export class ToggleService {
         },
         {
             id: "toggle_3",
-            title: "With right label and description",
+            title: "List",
             code: 
                 `
                 <div class="form-control">
 
-                <div class="toggle-group reverse">
+                <div class="toggle-group">
           
                   <div class="toggle-item">
                     <div>
@@ -143,12 +143,12 @@ export class ToggleService {
         },
         {
           id: "toggle_4",
-          title: "With right label inline",
+          title: "With label inline",
           code: 
               `
               <div class="form-control">
 
-              <div class="toggle-group inline reverse">
+              <div class="toggle-group inline">
         
                 <div class="toggle-item">
                   <div>
@@ -175,7 +175,30 @@ export class ToggleService {
                   </div>
                 </div>
         
-                <!-- item -->
+                <div class="toggle-item">
+                  <div>
+                    <span>Annual billing</span>
+                    <span>(Save 10%)</span>
+                  </div>
+                  <div class="toggle" appToggle>
+                    <div class="isActive">
+                      <button type="button" class="toggle-btn" role="switch" aria-checked="false">
+                        <span class="sr-only">Use setting</span>
+                        <span aria-hidden="true" class="toggle-shape"></span>
+                        <span aria-hidden="true" class="toggle-btn-default enabled"></span>
+                        <span aria-hidden="true" class="toggle-switch enabled"></span>
+                      </button>
+                    </div>
+                    <div class="inActive">
+                      <button type="button" class="toggle-btn" role="switch" aria-checked="false">
+                        <span class="sr-only">Use setting</span>
+                        <span aria-hidden="true" class="toggle-shape"></span>
+                        <span aria-hidden="true" class="toggle-btn-default not-enabled"></span>
+                        <span aria-hidden="true" class="toggle-switch not-enabled"></span>
+                      </button>
+                    </div>
+                  </div>
+                </div>
         
               </div>
         

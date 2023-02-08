00d0a069f102cf396ac657f67f6a2caecea4e618
cb-jayaraje <jjayaraaj@gmail.com>
2023-02-08 12:45:54 +0530

    Updated button, input

--- src/app/services/buttons.service.ts
+++ src/app/services/buttons.service.ts
@@ -120,7 +120,7 @@ export class ButtonService {
 
           <!--outline button with icon-->
           <div>
-            <butto class="btn btn-secondary btn-icon">
+            <button class="btn btn-secondary btn-icon">
               <svg class="btn-icon-leading" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                 fill="currentColor" aria-hidden="true">
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
@@ -222,7 +222,7 @@ export class ButtonService {
 
           <!--outline button with icon-->
           <div>
-            <butto class="btn btn-outline btn-icon">
+            <button class="btn btn-outline btn-icon">
               <svg class="btn-icon-leading" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                 fill="currentColor" aria-hidden="true">
                 <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
@@ -234,7 +234,7 @@ export class ButtonService {
 
           <!--outline button with trailing icon-->
           <div>
-            <butto class="btn btn-outline btn-icon">
+            <button class="btn btn-outline btn-icon">
             Button
               <svg class="btn-icon-trailing" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
                 fill="currentColor" aria-hidden="true">
@@ -348,7 +348,7 @@ export class ButtonService {
 
           <!--borderless icon circular-->
           <div>
-            <button class="btn-rounded btn btn-borderless">
+            <button class="btn-rounded btn-borderless">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                 <path fill-rule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
@@ -392,7 +392,7 @@ export class ButtonService {
 
           <!--borderless icon circular-->
           <div>
-            <button class="btn-rounded-small btn btn-borderless">
+            <button class="btn-rounded-small btn-borderless">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
                 <path fill-rule="evenodd"
                   d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
--- sting-components/button.js
+++ sting-components/button.js
@@ -1,10 +1,10 @@
 module.exports = Button = (styles) => ({
   " .btn": {
-    "@apply antialiased whitespace-nowrap items-center rounded border border-transparent bg-white px-4 py-2 font-medium cb-text-copy-regular  focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30 cursor-pointer items-start whitespace-nowrap":
+    "@apply antialiased rounded border border-transparent bg-white px-4 py-2 font-medium cb-text-copy-regular focus:outline-none disabled:cursor-not-allowed disabled:opacity-30 cursor-pointer":
       {},
   },
   ".btn-small": {
-    "@apply antialiased px-2.5 py-1.5 cb-text-copy-small border border-transparent font-medium rounded focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30":
+    "@apply antialiased px-2.5 py-1.5 cb-text-copy-small border border-transparent font-medium rounded focus:outline-none disabled:cursor-not-allowed disabled:opacity-30 cursor-pointer":
       {},
   },
   ".btn-rounded": {
@@ -43,33 +43,33 @@ module.exports = Button = (styles) => ({
   },
 
   // Sting theme
-  ".theme-sting .btn-primary": {
+  ".btn-primary, .theme-sting .btn-primary": {
     "@apply text-primary-50 bg-primary-800 hover:bg-primary-900 active:bg-primary-800 focus:ring-info-800 border border-transparent":
       {},
   },
-  ".theme-sting .btn-secondary": {
-    "@apply text-primary-600 hover:text-primary-700 bg-primary-50 hover:bg-primary-100 active:bg-primary-600 focus:outline-none border border-primary-100 hover:border-primary-200":
+  ".theme-sting .btn-secondary, .btn-secondary": {
+    "@apply text-primary-600 hover:text-primary-700 bg-primary-50 hover:bg-primary-100 active:bg-primary-50 focus:outline-none border border-primary-100 hover:border-primary-200":
       {},
   },
-  ".theme-sting .btn-outline": {
+  ".theme-sting .btn-outline, .btn-outline": {
     "@apply text-primary-600 hover:text-primary-700 bg-transparent hover:bg-primary-50 active:bg-transparent focus:outline-none border border-primary-500":
       {},
   },
-  ".theme-sting .btn-borderless": {
+  ".theme-sting .btn-borderless, .btn-borderless": {
     "@apply bg-transparent text-primary-600 hover:bg-primary-50": {},
   },
   // '.theme-sting .btn-info': {
   //     '@apply text-white bg-info-800 hover:bg-info-900 active:bg-info-800 hover:text-white focus:outline-none border border-transparent border-b-info-900': {},
   // },
-  ".theme-sting .btn-neutral": {
-    "@apply bg-transparent hover:bg-neutral-50 text-neutral-600 hover:text-neutral-700 active:bg-neutral-50 border border-neutral-200 hover:border-neutral-300":
+  ".theme-sting .btn-neutral, .btn-neutral": {
+    "@apply bg-transparent hover:bg-neutral-50 text-neutral-600 hover:text-neutral-700 active:bg-neutral-50 border border-neutral-200 hover:border-neutral-300 focus:ring-neutral-200":
       {},
   },
-  ".theme-sting .btn-neutral-borderless": {
+  ".theme-sting .btn-neutral-borderless, .btn-neutral-borderless": {
     "@apply bg-transparent text-neutral-600 hover:bg-neutral-50": {},
   },
-  ".theme-sting .btn-danger": {
-    "@apply text-red-600 bg-transparent hover:bg-red-50 border border-red-200 hover:border-red-300":
+  ".theme-sting .btn-danger, .btn-danger": {
+    "@apply text-red-600 bg-transparent hover:bg-red-50 border border-red-200 hover:border-red-300 focus:ring-red-400":
       {},
   },
 

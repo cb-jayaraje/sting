1d3e3d922e25e388fd02e5cfe6cd0f9094f1d03c
cb-jayaraje <jjayaraaj@gmail.com>
2023-03-29 14:43:41 +0530

    New UI ver 2

--- src/app/components/page/table/table.component.ts
+++ src/app/components/page/table/table.component.ts
@@ -6,47 +6,40 @@ import { TableService } from 'src/app/services/table.service';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
 })
 export class TableComponent implements OnInit {
-
   tables: any;
   convertedDatas: any = [];
   html: SafeHtml = '';
   constructor(
     private tableService: TableService,
-    public sanitizer: DomSanitizer, private titleService: Title,
-  ) { 
-    this.titleService.setTitle("Sting - Tables");
+    public sanitizer: DomSanitizer,
+    private titleService: Title
+  ) {
+    this.titleService.setTitle('Sting - Tables');
   }
 
   ngOnInit(): void {
-    this.tables =  this.tableService.tableDatas;
-
+    this.tables = this.tableService.tableDatas;
 
     from(this.tables)
-    .pipe(
-      map((data:any) => {
-        this.html = data.code;
-      //  console.log(this.html)
-        const datas = {
-         id: data.id,
-         title: data.title,
-         code:this.html
-        }
- 
-        return datas;
- 
-        
-        
-      })
-    )
-     .subscribe(dataRes => {
-       this.convertedDatas.push(dataRes)
-       // console.log(this.convertedDatas)
-     })
+      .pipe(
+        map((data: any) => {
+          this.html = data.code;
+          //  console.log(this.html)
+          const datas = {
+            id: data.id,
+            title: data.title,
+            code: this.html,
+          };
+
+          return datas;
+        })
+      )
+      .subscribe((dataRes: any) => {
+        this.convertedDatas.push(dataRes);
+        // console.log(this.convertedDatas)
+      });
   }
-
-
-
 }
